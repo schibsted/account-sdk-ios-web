7321786ec29aa6bc2fd4b108c7ca8a6008f63248
Samuel Gulliksson <samuel.gulliksson@schibsted.com>
2020-11-07 14:28:18 +0100

    Move KeychainSessionStorage to separate file.

---
+++ Sources/AccountSDKIOSWeb/Storage/Keychain/KeychainSessionStorage.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+internal class KeychainSessionStorage: SessionStorage {
+    private let keychain: KeychainStorage
+    
+    init(service: String, accessGroup: String? = nil) {
+        self.keychain = KeychainStorage(forService: service, accessGroup: accessGroup)
+    }
+    
+    func store(_ value: UserSession) {
+        guard let tokenData = try? JSONEncoder().encode(value) else {
+             fatalError("Failed to JSON encode user tokens for storage")
+        }
+        
+        keychain.addValue(tokenData, forAccount: value.clientId)
+    }
+
+    func get(forClientId: String) -> UserSession? {
+        guard let data = keychain.getValue(forAccount: forClientId),
+              let tokenData = try? JSONDecoder().decode(UserSession.self, from: data) else {
+            return nil
+        }
+        return tokenData
+    }
+    
+    func getAll() -> [UserSession] {
+        let data = keychain.getAll()
+        return data.compactMap { try? JSONDecoder().decode(UserSession.self, from: $0) }
+    }
+
+    func remove(forClientId: String) {
+        keychain.removeValue(forAccount: forClientId)
+    }
+}
