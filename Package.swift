1c13ca086ab0a4a0472b65c8eb45601dc61e390b
Samuel Gulliksson <samuel.gulliksson@schibsted.com>
2020-12-01 15:23:16 +0100

    Initial Travis setup. (#1)
    Run the unit tests.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["AccountSDKIOSWeb"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/airsidemobile/JOSESwift.git", from: "2.2.1"),
+        .package(url: "https://github.com/airsidemobile/JOSESwift.git", from: "2.3.0"),
         .package(url: "https://github.com/Brightify/Cuckoo.git", from: "1.4.0"),
     ],
     targets: [
@@ -23,7 +23,7 @@ let package = Package(
             name: "AccountSDKIOSWebTests",
             dependencies: ["AccountSDKIOSWeb", "Cuckoo"]),
         .testTarget(
-            name: "AccountSDKIOSKeychainTests",
+            name: "AccountSDKIOSWebKeychainTests",
             dependencies: ["AccountSDKIOSWeb"]),
     ]
 )
