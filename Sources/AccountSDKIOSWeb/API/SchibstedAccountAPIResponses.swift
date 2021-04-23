1d82f28f7761a01e8cdff09c6edd11f3f99487d8
samuel.gulliksson@schibsted.com <samuel.gulliksson@schibsted.com>
2021-04-23 09:23:10 +0200

    Parse full user profile data response. (#30)

--- Sources/AccountSDKIOSWeb/API/SchibstedAccountAPIResponses.swift
+++ Sources/AccountSDKIOSWeb/API/SchibstedAccountAPIResponses.swift
@@ -8,16 +8,159 @@ struct SessionExchangeResponse: Codable {
     let code: String
 }
 
+public struct UserProfileResponse: Codable, Equatable {
+    public var uuid: String? = nil
+    public var userId: String? = nil
+    public var status: Int? = nil
+    public var email: String? = nil
+    internal var emailVerified: MaybeString? = nil
+    public var emails: [Email]? = nil
+    public var phoneNumber: String? = nil
+    internal var phoneNumberVerified: MaybeString? = nil
+    public var phoneNumbers: [PhoneNumber]? = nil
+    public var displayName: String? = nil
+    public var name: Name? = nil
+    public var addresses: [String: Address]? = nil
+    public var gender: String? = nil
+    internal var birthday: String?
+    public var accounts: [String: Account]? = nil
+    public var merchants: [Int]? = nil
+    public var published: String? = nil
+    public var verified: String? = nil
+    public var updated: String? = nil
+    public var passwordChanged: String? = nil
+    public var lastAuthenticated: String? = nil
+    public var lastLoggedIn: String? = nil
+    public var locale: String? = nil
+    public var utcOffset: String? = nil
+
+    public var emailVerifiedDate: String? {
+        get { emailVerified?.value }
+    }
+    
+    public var phoneNumberVerifiedDate: String? {
+        get { phoneNumberVerified?.value }
+    }
+    
+    public var birthdate: String? {
+        get {
+            if birthday == "0000-00-00" {
+                return nil
+            }
+            return birthday
+        }
+    }
+}
+
+public protocol Identifier: Codable, Equatable {
+    var value: String? { get }
+    var type: String?  { get }
+    var isPrimary: Bool? { get }
+    var isVerified: Bool? { get }
+    var verifiedTime: String? { get }
+}
+
+public struct Email: Identifier {
+    public var value: String? = nil
+    public var type: String? = nil
+    internal var primary: StringBool? = nil
+    internal var verified: StringBool? = nil
+    public var verifiedTime: String? = nil
+    
+    public var isPrimary: Bool? { get { primary?.value } }
+    public var isVerified: Bool? { get { verified?.value } }
+}
+
+public struct PhoneNumber: Identifier {
+    public var value: String? = nil
+    public var type: String? = nil
+    internal var primary: StringBool? = nil
+    internal var verified: StringBool? = nil
+    public var verifiedTime: String? = nil
+    
+    public var isPrimary: Bool? { get { primary?.value } }
+    public var isVerified: Bool? { get { verified?.value } }
+}
+
 public struct Name: Codable, Equatable {
     public var givenName: String? = nil
     public var familyName: String? = nil
     public var formatted: String? = nil
 }
 
-public struct UserProfileResponse: Codable, Equatable {
-    public var userId: String? = nil
-    public var email: String? = nil
-    public var phoneNumber: String? = nil
-    public var displayName: String? = nil
-    public var name: Name? = nil
+public struct Account: Codable, Equatable {
+    public var id: String? = nil
+    public var accountName: String? = nil
+    public var domain: String? = nil
+    public var connected: String? = nil
+}
+
+public struct Address: Codable, Equatable {
+    public var formatted: String? = nil
+    public var streetAddress: String? = nil
+    public var postalCode: String? = nil
+    public var locality: String? = nil
+    public var region: String? = nil
+    public var country: String? = nil
+    public var type: AddressType? = nil
+
+    public enum AddressType: String, Codable, Equatable {
+        case home = "home"
+        case delivery = "delivery"
+        case invoice = "invoice"
+    }
+}
+
+internal struct StringBool: Codable, Equatable {
+    let value: Bool?
+    let asString: Bool
+}
+
+extension StringBool {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        guard let value = try? container.decode(Bool.self) else {
+            let value = try? container.decode(String.self)
+            self.value = value == "true"
+            asString = true
+            return
+        }
+        
+        self.value = value
+        asString = false
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        guard let boolValue = value else {
+            return
+        }
+
+        var container = encoder.singleValueContainer()
+        if (asString) {
+            try container.encode(String(boolValue))
+        } else {
+            try container.encode(boolValue)
+        }
+    }
+}
+
+internal struct MaybeString: Codable, Equatable {
+    let value: String?
+}
+
+extension MaybeString {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        self.value = try? container.decode(String.self)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        guard let stringValue = value else {
+            try container.encode(false)
+            return
+        }
+
+        try container.encode(stringValue)
+    }
 }
---
+++ Tests/AccountSDKIOSWebTests/API/SchibstedAccountAPIResponsesTests.swift
@@ -0,0 +1,117 @@
+import XCTest
+@testable import AccountSDKIOSWeb
+
+final class SchibstedAccountAPIResponsesTests: XCTestCase {
+    func testMaybeStringParsesStringValue() throws {
+        let value = "testStringValue"
+        let parsed = try! JSONDecoder().decode(MaybeString.self, from: "\"\(value)\"".data(using: .utf8)!)
+        XCTAssertEqual(parsed.value, value)
+    }
+    
+    func testMaybeStringIgnoresBoolValue() {
+        let parsed = try! JSONDecoder().decode(MaybeString.self, from: "false".data(using: .utf8)!)
+        XCTAssertNil(parsed.value)
+    }
+    
+    func testStringBool() {
+        let jsonDecoder = JSONDecoder()
+        func decode(_ value: String) -> StringBool? {
+            return try! jsonDecoder.decode(StringBool.self, from: value.data(using: .utf8)!)
+        }
+
+        XCTAssertEqual(decode("false"), StringBool(value: false, asString: false))
+        XCTAssertEqual(decode("true"), StringBool(value: true, asString: false))
+        XCTAssertEqual(decode("\"false\""), StringBool(value: false, asString: true))
+        XCTAssertEqual(decode("\"true\""), StringBool(value: true, asString: true))
+        XCTAssertEqual(decode("\"notTrue\""), StringBool(value: false, asString: true))
+    }
+    
+    func testUserProfileResponseHandlesBoolInStringFields() {
+        let json = """
+        {
+            "emailVerified": false,
+            "phoneNumberVerified": false
+        }
+        """
+        let parsed = try! JSONDecoder().decode(UserProfileResponse.self, from: json.data(using: .utf8)!)
+        XCTAssertNil(parsed.emailVerifiedDate)
+        XCTAssertNil(parsed.phoneNumberVerifiedDate)
+    }
+    
+    func testUserProfileResponseFiltersEmptyBirthdayValue() {
+        let json = """
+        {
+            "birthday": "0000-00-00"
+        }
+        """
+        let parsed = try! JSONDecoder().decode(UserProfileResponse.self, from: json.data(using: .utf8)!)
+        XCTAssertNil(parsed.birthdate)
+    }
+    
+    func testFullProfileResponse() {
+        guard let filePath = Bundle(for: type(of: self)).path(forResource: "user-profile-response", ofType: "json"),
+              let jsonString = try? String(contentsOfFile: filePath, encoding: .utf8) else {
+            XCTFail("Failed to read user_profile_response.json")
+            return
+        }
+
+        let expectedResponse = UserProfileResponse(
+            uuid: "96085e85-349b-4dbf-9809-fa721e7bae46",
+            userId: "12345",
+            status: 1,
+            email: "test@example.com",
+            emailVerified: MaybeString(value: "1970-01-01 00:00:00"),
+            emails: [
+                Email(
+                    value: "test@example.com",
+                    type: "other",
+                    primary: StringBool(value: true, asString: true),
+                    verified: StringBool(value: true, asString: true),
+                    verifiedTime: "1970-01-01 00:00:00"
+                )
+            ],
+            phoneNumber: "+46123456",
+            phoneNumberVerified: MaybeString(value: nil),
+            phoneNumbers: [
+                PhoneNumber(
+                    value: "+46123456",
+                    type: "other",
+                    primary: StringBool(value: false, asString: true),
+                    verified: StringBool(value: false, asString: true)
+                )
+            ],
+            displayName: "Unit test",
+            name: Name(givenName: "Unit", familyName: "Test", formatted: "Unit Test"),
+            addresses: [
+                "home" : Address(
+                    formatted: "12345 Test, Sverige",
+                    streetAddress: "Test",
+                    postalCode: "12345",
+                    locality: "Test locality",
+                    region: "Test region",
+                    country: "Sverige",
+                    type: .home
+                )
+            ],
+            gender: "withheld",
+            birthday: "1970-01-01 00:00:00",
+            accounts: [
+                "client1": Account(id: "client1", accountName: "Example", domain: "example.com", connected: "1970-01-01 00:00:00")
+            ],
+            merchants: [12345, 54321],
+            published: "1970-01-01 00:00:00",
+            verified: "1970-01-01 00:00:00",
+            updated: "1971-01-01 00:00:00",
+            passwordChanged: "1970-01-01 00:00:00",
+            lastAuthenticated: "1970-01-01 00:00:00",
+            lastLoggedIn: "1970-01-01 00:00:00",
+            locale: "sv_SE",
+            utcOffset: "+02:00"
+        )
+        
+        let parsed = try! JSONDecoder().decode(UserProfileResponse.self, from: jsonString.data(using: .utf8)!)
+        print(String(data: try! JSONEncoder().encode(parsed), encoding: .utf8)!)
+        print(String(data: try! JSONEncoder().encode(expectedResponse), encoding: .utf8)!)
+        XCTAssertEqual(parsed, expectedResponse)
+    }
+}
