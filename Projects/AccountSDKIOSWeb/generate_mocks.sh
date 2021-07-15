e234b43755cdd8f46c0c9e7e1a5444aebc41ef2d
Daniel Echegaray <daniel.echegaray@schibsted.com>
2021-07-15 16:16:31 +0200

    1024 - Cucko generating new mocks

--- Projects/AccountSDKIOSWeb/generate_mocks.sh
+++ Projects/AccountSDKIOSWeb/generate_mocks.sh
@@ -1,9 +1,9 @@
 ./cuckoo/run generate --testable AccountSDKIOSWeb \
   --output Tests/AccountSDKIOSWebTests/GeneratedMocks.swift \
-  Sources/AccountSDKIOSWeb/HTTP/HTTPClient.swift \
-  Sources/AccountSDKIOSWeb/HTTP/URLSessionProtocol.swift \
-  Sources/AccountSDKIOSWeb/Storage/SessionStorage.swift \
-  Sources/AccountSDKIOSWeb/Storage/Storage.swift \
-  Sources/AccountSDKIOSWeb/Storage/Keychain/Compat/LegacyKeychainSessionStorage.swift \
-  Sources/AccountSDKIOSWeb/Storage/Keychain/Compat/LegacyKeychainTokenStorage.swift \
-  Sources/AccountSDKIOSWeb/Storage/Keychain/KeychainSessionStorage.swift
+    ../../Sources/AccountSDKIOSWeb/Lib/HTTP/HTTPClient.swift \
+    ../../Sources/AccountSDKIOSWeb/Lib/HTTP/URLSessionProtocol.swift \
+    ../../Sources/AccountSDKIOSWeb/Lib/Storage/SessionStorage.swift \
+    ../../Sources/AccountSDKIOSWeb/Lib/Storage/Storage.swift \
+    ../../Sources/AccountSDKIOSWeb/Lib/Storage/Keychain/Compat/LegacyKeychainSessionStorage.swift \
+    ../../Sources/AccountSDKIOSWeb/Lib/Storage/Keychain/Compat/LegacyKeychainTokenStorage.swift \
+    ../../Sources/AccountSDKIOSWeb/Lib/Storage/Keychain/KeychainSessionStorage.swift
