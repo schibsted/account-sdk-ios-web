cee3fa2ff73fd0d40711829c7c2f468dcc50740f
Daniel Echegaray <daniel.echegaray@schibsted.com>
2021-07-21 16:13:51 +0200

    ExampleApp AccountPagesURL fixup

--- Projects/AccountSDKIOSWebExample/ExampleWeb/ExampleWebApp.swift
+++ Projects/AccountSDKIOSWebExample/ExampleWeb/ExampleWebApp.swift
@@ -4,18 +4,21 @@ import AccountSDKIOSWeb
 @main
 struct ExampleWebApp: App {
     private let client: Client
+    private let clientConfiguration: ClientConfiguration
     
     init() {
         let clientRedirectURI = URL(string: "com.sdk-example.pre.602504e1b41fa31789a95aa7:/login")!
         let clientConfiguration = ClientConfiguration(environment: .pre,
                                                       clientId: "602504e1b41fa31789a95aa7",
                                                       redirectURI: clientRedirectURI)
-        client = Client(configuration: clientConfiguration)
+        
+        self.client = Client(configuration: clientConfiguration)
+        self.clientConfiguration = clientConfiguration
     }
 
     var body: some Scene {
         WindowGroup {
-            ContentView(client: client)
+            ContentView(client: client, clientConfiguration: clientConfiguration)
         }
     }
 }
