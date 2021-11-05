77a852c758fdfafb66338b6c58e51d3ceea22c7f
Anna Zamora <anna.zamora@schibsted.com>
2021-11-05 14:31:46 +0100

    1748 Cleanup ExampleApp main view

--- Projects/AccountSDKIOSWebExample/ExampleWeb/ContentView.swift
+++ Projects/AccountSDKIOSWebExample/ExampleWeb/ContentView.swift
@@ -21,161 +21,154 @@ struct ContentView: View {
             user?.isLoggedIn() ?? false
         }
     }
-
+    
     @State private var accountPagesURL: URL?
     @State private var showAccountPages = false
-
     @State private var asWebAuthSession: ASWebAuthenticationSession?
-
+    
     init(client: Client, clientConfiguration: ClientConfiguration) {
         self.client = client
         self.clientConfiguration = clientConfiguration
     }
-
+    
     var body: some View {
         NavigationView {
             let webView = WebView(url: $accountPagesURL)
-
-            VStack(spacing: 20) {
-                Group {
-                    Text(String(describing: client))
-                    Text("Logged-in as \(user?.uuid ?? "unknown")")
-                }
-                
-                Button(action: {
-                    client.resumeLastLoggedInUser() { user in
-                        guard let user = user else {
-                            print("User could not be resumed")
-                            return
-                        }
-                        self.user = user
-                        print("Resumed user")
+            
+            VStack(spacing: 50) {
+                VStack(spacing: 20) {
+                    Group {
+                        Text(String(describing: client))
+                        Text("Logged-in as \(user?.uuid ?? "unknown")")
                     }
-                }) {
-                    Text("Resume user")
-                }
-
-                Group {
-                    Button(action: {
-                        let context = ASWebAuthSessionContextProvider()
-                        asWebAuthSession = client.getLoginSession(contextProvider: context,
-                                                                  withMFA: .otp,
-                                                                  withSSO: true,
-                                                                  completion: { result in
-                            switch result {
-                            case .success(let user):
-                                print("Success - logged in as \(user.uuid ?? "")")
-                                self.user = user
-                            case .failure(let error):
-                                print(error)
-                            }
-                        })
-                        
-                        // This will trigger the web context asking the user to login
-                        asWebAuthSession?.start()
-       
-                    }) {
-                        Text("Trigger 2FA (OTP)")
-                    }
+                    Button(action: resumeUser, label: { Text("Resume user") } )
+                    Button(action: trigger2faOtpFlow, label: { Text("Trigger 2FA (OTP)") } )
+                    Button(action: trigger2faSmsFlow, label: { Text("Trigger 2FA (SMS)") } )
                     
-                    Button(action: {
-                        
-                        let context = ASWebAuthSessionContextProvider()
-                        asWebAuthSession = client.getLoginSession(contextProvider: context,
-                                                                  withMFA: .sms,
-                                                                  withSSO: true) { result in
-                            switch result {
-                            case .success(let user):
-                                print("Success - logged in as \(user.uuid ?? "")")
-                                self.user = user
-                            case .failure(let error):
-                                print(error)
-                            }
+                    Button(action: login, label: { Text("Login") } )
+                        .onOpenURL { url in
+                            handleOnOpenUrl(url: url)
                         }
-                        asWebAuthSession?.start()
-                        
-                    }) {
-                        Text("Trigger 2FA (SMS)")
-                    }
                 }
-
-                let loginButton = Button(action: {
-                    let context = ASWebAuthSessionContextProvider()
-                    asWebAuthSession = client.getLoginSession(contextProvider: context,
-                                                              withSSO: true,
-                                                              completion: handleResult)
-                    asWebAuthSession?.start()
-                }) {
-                    Text("Login")
-                }
-
-                loginButton.onOpenURL { url in
-                    client.handleAuthenticationResponse(url: url) { result in
-                        DispatchQueue.main.async {
-                            asWebAuthSession?.cancel()
-                        }
-
-                        handleResult(result)
-                    }
-                }
-               
-                Spacer().frame(height: 50)
                 
-                Button(action: {
-                    self.user?.fetchProfileData { result in
-                        switch result {
-                        case .success(let userData):
-                            print(userData)
-                        case .failure(.unexpectedError(LoginStateError.notLoggedIn)):
-                            print("User was logged-out")
-                            self.user = nil
-                        case .failure(let error):
-                            print(error)
-                        }
-                    }
-                }) {
-                    Text("Fetch profile data")
+                VStack(spacing: 20) {
+                    Button(action: fetchProfileData, label: { Text("Fetch profile data") } )
+                    Button(action: startSessionExchange, label: { Text("Start session exchange") } )
+                    Button(action: openAccountPages, label: { Text("Show account pages") } )
+                    Button(action: logout, label: { Text("Logout") } )
+                    
                 }.disabled(!userIsLoggedIn)
                 
-                Button(action: {
-                    self.user?.webSessionURL(clientId: "602504e1b41fa31789a95aa7", redirectURI: "http://zoopermarket.com/safepage") { result in
-                        switch result {
-                        case .success(let sessionUrl):
-                            print(sessionUrl)
-                        case .failure(let error):
-                            print(error)
-                        }
-                    }
-                }) {
-                    Text("Start session exchange")
-                }.disabled(!userIsLoggedIn)
-
-                Button(action: {
-                    self.user?.webSessionURL(clientId: "602504e1b41fa31789a95aa7", redirectURI: self.clientConfiguration.accountPagesURL.absoluteString) { result in
-                        switch result {
-                        case .success(let sessionUrl):
-                            print(sessionUrl)
-                            accountPagesURL = sessionUrl
-                            showAccountPages = true
-                        case .failure(let error):
-                            print(error)
-                        }
-                    }
-
-                }) {
-                    Text("Show account pages")
-                }.disabled(!userIsLoggedIn)
-                
-                Button(action: {
-                    self.user?.logout()
-                    self.user = nil
-                    print("Logged out")
-                }) {
-                    Text("Logout")
-                }.disabled(!userIsLoggedIn)
                 NavigationLink("", destination: webView, isActive: $showAccountPages)
             }
         }
+    }
+    
+    func resumeUser() {
+        client.resumeLastLoggedInUser() { user in
+            guard let user = user else {
+                print("User could not be resumed")
+                return
+            }
+            self.user = user
+            print("Resumed user")
+        }
+    }
+    
+    func trigger2faOtpFlow() {
+        let context = ASWebAuthSessionContextProvider()
+        asWebAuthSession = client.getLoginSession(contextProvider: context,
+                                                  withMFA: .otp,
+                                                  withSSO: true,
+                                                  completion: { result in
+            switch result {
+            case .success(let user):
+                print("Success - logged in as \(user.uuid ?? "")")
+                self.user = user
+            case .failure(let error):
+                print(error)
+            }
+        })
+        
+        // This will trigger the web context asking the user to login
+        asWebAuthSession?.start()
+    }
+    
+    func trigger2faSmsFlow() {
+        let context = ASWebAuthSessionContextProvider()
+        asWebAuthSession = client.getLoginSession(contextProvider: context,
+                                                  withMFA: .sms,
+                                                  withSSO: true) { result in
+            switch result {
+            case .success(let user):
+                print("Success - logged in as \(user.uuid ?? "")")
+                self.user = user
+            case .failure(let error):
+                print(error)
+            }
+        }
+        asWebAuthSession?.start()
+    }
+    
+    func login() {
+        let context = ASWebAuthSessionContextProvider()
+        asWebAuthSession = client.getLoginSession(contextProvider: context,
+                                                  withSSO: true,
+                                                  completion: handleResult)
+        asWebAuthSession?.start()
+    }
+    
+    func handleOnOpenUrl(url: URL) {
+        client.handleAuthenticationResponse(url: url) { result in
+            DispatchQueue.main.async {
+                asWebAuthSession?.cancel()
+            }
+            handleResult(result)
+        }
+    }
+    
+    func fetchProfileData() {
+        self.user?.fetchProfileData { result in
+            switch result {
+            case .success(let userData):
+                print(userData)
+            case .failure(.unexpectedError(LoginStateError.notLoggedIn)):
+                print("User was logged-out")
+                self.user = nil
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func startSessionExchange() {
+        self.user?.webSessionURL(clientId: "602504e1b41fa31789a95aa7", redirectURI: "http://zoopermarket.com/safepage") { result in
+            switch result {
+            case .success(let sessionUrl):
+                print(sessionUrl)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func openAccountPages() {
+        self.user?.webSessionURL(clientId: "602504e1b41fa31789a95aa7", redirectURI: self.clientConfiguration.accountPagesURL.absoluteString) { result in
+            switch result {
+            case .success(let sessionUrl):
+                print(sessionUrl)
+                accountPagesURL = sessionUrl
+                showAccountPages = true
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func logout() {
+        self.user?.logout()
+        self.user = nil
+        print("Logged out")
     }
     
     func handleResult(_ result: Result<User, LoginError>) {
@@ -189,7 +182,6 @@ struct ContentView: View {
         }
     }
 }
-
 
 struct WebView : UIViewRepresentable {
     @Binding var url: URL?
