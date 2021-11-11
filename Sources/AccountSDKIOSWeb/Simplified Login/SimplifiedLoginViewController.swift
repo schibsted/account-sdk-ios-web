b1dd5d70c18254447e9b4e97d409f35552a77393
Daniel Echegaray <daniel.echegaray@schibsted.com>
2021-11-11 15:10:47 +0100

    1750 SimplifiedLogin ViewModel

--- Sources/AccountSDKIOSWeb/Simplified Login/SimplifiedLoginViewController.swift
+++ Sources/AccountSDKIOSWeb/Simplified Login/SimplifiedLoginViewController.swift
@@ -1,8 +1,72 @@
 import SwiftUI
 import UIKit
 
-public class SimplifiedLoginViewController: UIViewController {
+public struct SimplifiedLoginViewModel {
+    var localisation: Localisation
+    var iconNames: [String]
+    let schibstedLogoName = "sch-logo"
+    
+    let displayName = "Daniel.User" // TODO: Need to be fetched
+    let clientName = "Finn" // TODO: Need to be fetched
+    
+    init?(env: ClientConfiguration.Environment){
+        let resourceName: String
+        let orderedIconNames: [String]
+        switch env {
+        case .proCom:
+            resourceName = "for_use_simplified-widget_translations_sv"
+            orderedIconNames = ["Blocket", "Aftonbladet", "SVD", "Omni", "TvNu"]
+        case .proNo:
+            resourceName = "for_use_simplified-widget_translations_nb"
+            orderedIconNames = ["Finn", "VG", "Aftenposten", "E24", "BergensTidene"]
+        case .proFi:
+            resourceName = "for_use_simplified-widget_translations_fi"
+            orderedIconNames = ["Tori", "Oikotie", "Hintaopas", "Lendo", "Rakentaja"]
+        case .proDk:
+            resourceName = "for_use_simplified-widget_translations_da"
+            orderedIconNames = ["Tori", "Oikotie", "Hintaopas", "Lendo", "Rakentaja"] //TODO: NEED DK 5 brands with icons
+        case .pre:
+            resourceName = "simplified-widget_translations_en"
+            orderedIconNames = ["Blocket", "Aftonbladet", "SVD", "Omni", "TvNu"] // Using SV icons
+        }
         
+        let decoder = JSONDecoder()
+        guard
+            let url = Bundle(for: SimplifiedLoginViewController.self).url(forResource: resourceName, withExtension: "json"),
+            let data = try? Data(contentsOf: url),
+            let localisation = try? decoder.decode(Localisation.self, from: data)
+        else {
+            return nil
+        }
+        
+        self.localisation = localisation
+        self.iconNames = orderedIconNames
+    }
+
+    struct Localisation: Codable {
+        var schibstedTitle: String
+        var continueWithoutLogin: String
+        var explanationText: String
+        var privacyPolicyTitle: String
+        var privacyPolicyURL: String
+        var switchAccoutn: String
+        var notYouTitle: String
+        var continuAsButtonTitle: String
+        
+        enum CodingKeys: String, CodingKey {
+            case schibstedTitle = "SimplifiedWidget.schibstedAccount"
+            case continueWithoutLogin = "SimplifiedWidget.continueWithoutLogin"
+            case explanationText = "SimplifiedWidget.footer"
+            case privacyPolicyTitle = "SimplifiedWidget.privacyPolicy"
+            case privacyPolicyURL = "SimplifiedWidget.privacyPolicyLink"
+            case switchAccoutn = "SimplifiedWidget.loginWithDifferentAccount"
+            case notYouTitle = "SimplifiedWidget.notYou"
+            case continuAsButtonTitle = "SimplifiedWidget.continueAs"
+        }
+    }
+}
+
+public class SimplifiedLoginViewController: UIViewController {
     private lazy var userInformationView: UserInformationView = {
         let view = UserInformationView()
         view.alignment = .center
