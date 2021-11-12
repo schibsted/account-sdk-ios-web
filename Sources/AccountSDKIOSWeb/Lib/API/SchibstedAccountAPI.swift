43344b0057f47a705a3f237ef931d8813f4e7344
Anna Zamora <anna.zamora@schibsted.com>
2021-11-12 11:31:44 +0100

    1774 Apply review changes

--- Sources/AccountSDKIOSWeb/Lib/API/SchibstedAccountAPI.swift
+++ Sources/AccountSDKIOSWeb/Lib/API/SchibstedAccountAPI.swift
@@ -69,7 +69,7 @@ class SchibstedAccountAPI {
         return requestWithHeaders
     }
     
-    func sessionExchange(for user: User, state: String? = nil, clientId: String, redirectURI: String, completion: @escaping HTTPResultHandler<SessionExchangeResponse>) {
+    func sessionExchange(for user: User, clientId: String, redirectURI: String, state: String? = nil, completion: @escaping HTTPResultHandler<SessionExchangeResponse>) {
         let url = baseURL.appendingPathComponent("/api/2/oauth/exchange")
         var parameters = [
             "type": "session",
