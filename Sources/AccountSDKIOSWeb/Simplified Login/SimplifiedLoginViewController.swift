cf3f4e7a5d77295ecc6d75473707af0f54eee52e
Daniel Echegaray <daniel.echegaray@schibsted.com>
2021-11-16 16:46:02 +0100

    1770 Adding simple in-app-webview

--- Sources/AccountSDKIOSWeb/Simplified Login/SimplifiedLoginViewController.swift
+++ Sources/AccountSDKIOSWeb/Simplified Login/SimplifiedLoginViewController.swift
@@ -1,5 +1,24 @@
 import SwiftUI
 import UIKit
+import WebKit
+
+class WebViewController: UIViewController, WKNavigationDelegate {
+
+    let webView: WKWebView
+    
+    override func loadView() {
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    init(url: URL) {
+        webView = WKWebView()
+        webView.load(URLRequest(url: url))
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
 
 public struct SimplifiedLoginUIFactory {
 
@@ -24,7 +43,12 @@ public struct SimplifiedLoginUIFactory {
         viewModel.onClickedContinueWithoutLogin = {
             s.dismiss(animated: true, completion: nil)
         }
-        viewModel.onClickedPrivacyPolicy = {}  // TODO: Setup webview
+        
+        viewModel.onClickedPrivacyPolicy = {
+            let url = URL(string: viewModel.localisation.privacyPolicyURL)!
+            let webVC = WebViewController(url: url)
+            s.present(webVC, animated: false, completion: nil) // TODO: NEED to present in NC
+        }
         
         return s
     }
