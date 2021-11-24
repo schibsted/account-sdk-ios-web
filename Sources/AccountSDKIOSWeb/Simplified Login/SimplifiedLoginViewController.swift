a3154d4ca74da979df9561cbf5ce7489d73838be
Daniel Echegaray <daniel.echegaray@schibsted.com>
2021-11-24 16:37:31 +0100

    1816 Adding todo

--- Sources/AccountSDKIOSWeb/Simplified Login/SimplifiedLoginViewController.swift
+++ Sources/AccountSDKIOSWeb/Simplified Login/SimplifiedLoginViewController.swift
@@ -195,7 +195,7 @@ public struct SimplifiedLoginViewControllerRepresentable: UIViewControllerRepres
         }
         
         let context = UserContextFromTokenResponse(identifier: "An identifier", display_text: "a display text", client_name: "A client name")
-        let profile = UserProfileResponse(locale: "sv_SE")
+        let profile = UserProfileResponse(locale: "sv_SE") // TODO: Shouldn't be hardcoded
         
         let s = SimplifiedLoginUIFactory.buildViewController(client: client, withMFA: .password, loginHint: nil, extraScopeValues: [], withSSO: true, userContext: context, profileResponse: profile, completion: completion) as! UINavigationController
         
