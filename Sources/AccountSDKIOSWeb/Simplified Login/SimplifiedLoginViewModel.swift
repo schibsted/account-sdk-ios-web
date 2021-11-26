617c6069795552a6c54a4ef1bec90f849e3362b2
Daniel Echegaray <daniel.echegaray@schibsted.com>
2021-11-26 11:49:15 +0100

    1838 Using system localization strings in ViewModel

--- Sources/AccountSDKIOSWeb/Simplified Login/SimplifiedLoginViewModel.swift
+++ Sources/AccountSDKIOSWeb/Simplified Login/SimplifiedLoginViewModel.swift
@@ -8,7 +8,6 @@ class SimplifiedLoginViewModel {
     var onClickedPrivacyPolicy: (() -> Void)?
     var onClickedContinueAsUser: (() -> Void)? // TODO:
     
-    var localisation: Localisation
     var iconNames: [String]
     let schibstedLogoName = "sch-logo"
     
@@ -18,39 +17,24 @@ class SimplifiedLoginViewModel {
     let client: Client
     var asWebAuthenticationSession: ASWebAuthenticationSession?
     
-    init?(client: Client, env: ClientConfiguration.Environment) {
+    init(client: Client, env: ClientConfiguration.Environment) {
         
         self.client = client
         
-        let resourceName: String
         let orderedIconNames: [String]
         switch env {
         case .proCom:
-            resourceName = "for_use_simplified-widget_translations_sv"
             orderedIconNames = ["Blocket", "Aftonbladet", "SVD", "Omni", "TvNu"]
         case .proNo:
-            resourceName = "for_use_simplified-widget_translations_nb"
             orderedIconNames = ["Finn", "VG", "Aftenposten", "E24", "BergensTidene"]
         case .proFi:
-            resourceName = "for_use_simplified-widget_translations_fi"
             orderedIconNames = ["Tori", "Oikotie", "Hintaopas", "Lendo", "Rakentaja"]
         case .proDk:
-            resourceName = "for_use_simplified-widget_translations_da"
             orderedIconNames = ["Tori", "Oikotie", "Hintaopas", "Lendo", "Rakentaja"] //TODO: NEED DK 5 brands with icons
         case .pre:
-            resourceName = "simplified-widget_translations_en"
             orderedIconNames = ["Blocket", "Aftonbladet", "SVD", "Omni", "TvNu"] // Using SV icons
         }
         
-        let decoder = JSONDecoder()
-        guard let url = Bundle.accountSDK(for: SimplifiedLoginViewModel.self).url(forResource: resourceName, withExtension: "json"),
-              let data = try? Data(contentsOf: url),
-              let localisation = try? decoder.decode(Localisation.self, from: data)
-        else {
-            return nil
-        }
-        
-        self.localisation = localisation
         self.iconNames = orderedIconNames
     }
     
@@ -66,26 +50,53 @@ class SimplifiedLoginViewModel {
             self.onClickedPrivacyPolicy?()
         }
     }
-    
-    struct Localisation: Codable {
-        var schibstedTitle: String
-        var continueWithoutLogin: String
-        var explanationText: String
-        var privacyPolicyTitle: String
-        var privacyPolicyURL: String
-        var switchAccount: String
-        var notYouTitle: String
-        var continuAsButtonTitle: String
+}
+
+extension SimplifiedLoginViewModel {
+    enum Localization: String {
+        case schibstedTitle = "SimplifiedWidget.schibstedAccount"
+        case continueWithoutLogin = "SimplifiedWidget.continueWithoutLogin"
+        case explanationText = "SimplifiedWidget.footer"
+        case privacyPolicyTitle = "SimplifiedWidget.privacyPolicy"
+        case privacyPolicyURL = "SimplifiedWidget.privacyPolicyLink"
+        case switchAccount = "SimplifiedWidget.loginWithDifferentAccount"
+        case notYouTitle = "SimplifiedWidget.notYou"
+        case continuAsButtonTitle = "SimplifiedWidget.continueAs"
         
-        enum CodingKeys: String, CodingKey {
-            case schibstedTitle = "SimplifiedWidget.schibstedAccount"
-            case continueWithoutLogin = "SimplifiedWidget.continueWithoutLogin"
-            case explanationText = "SimplifiedWidget.footer"
-            case privacyPolicyTitle = "SimplifiedWidget.privacyPolicy"
-            case privacyPolicyURL = "SimplifiedWidget.privacyPolicyLink"
-            case switchAccount = "SimplifiedWidget.loginWithDifferentAccount"
-            case notYouTitle = "SimplifiedWidget.notYou"
-            case continuAsButtonTitle = "SimplifiedWidget.continueAs"
+        var localizedString: String{
+            return self.rawValue.localized()
         }
+    }
+    
+    var schibstedTitle: String {
+        return Localization.schibstedTitle.localizedString
+    }
+    
+    var continueWithoutLogin: String {
+        return Localization.continueWithoutLogin.localizedString
+    }
+    
+    var explanationText: String {
+        return Localization.explanationText.localizedString
+    }
+    
+    var privacyPolicyTitle: String {
+        return Localization.privacyPolicyTitle.localizedString
+    }
+    
+    var privacyPolicyURL: String {
+        return Localization.privacyPolicyURL.localizedString
+    }
+    
+    var switchAccount: String {
+        return Localization.switchAccount.localizedString
+    }
+    
+    var notYouTitle: String {
+        return Localization.notYouTitle.localizedString
+    }
+    
+    var continuAsButtonTitle: String {
+        return Localization.continuAsButtonTitle.localizedString
     }
 }
