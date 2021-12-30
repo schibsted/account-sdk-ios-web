9cac68e626badbb3e2aa5ad6265ece691d1766c8
Anna Zamora <anna.zamora@schibsted.com>
2021-12-30 13:59:22 +0100

    1894 Add missing file content

--- Sources/AccountSDKIOSWeb/Util/KeyWindow.swift
+++ Sources/AccountSDKIOSWeb/Util/KeyWindow.swift
@@ -1,8 +1,16 @@
-//
-//  KeyWindow.swift
-//  AccountSDKIOSWeb
-//
-//  Created by Anna Zamora on 30/12/2021.
-//
+import UIKit
 
-import Foundation
+struct KeyWindow {
+    static func get() -> UIWindow? {
+        if #available(iOS 13.0, *) {
+            return UIApplication
+                .shared
+                .connectedScenes
+                .compactMap { $0 as? UIWindowScene }
+                .flatMap { $0.windows }
+                .first { $0.isKeyWindow }
+        } else {
+            return UIApplication.shared.keyWindow
+        }
+    }
+}
