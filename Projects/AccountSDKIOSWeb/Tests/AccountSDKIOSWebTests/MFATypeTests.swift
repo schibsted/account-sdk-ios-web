4d2339d4c5538023707d37c746e40f36463a5e47
Anna Zamora <anna.zamora@schibsted.com>
2022-03-03 13:24:31 +0100

    Remove unnecessary values

--- Projects/AccountSDKIOSWeb/Tests/AccountSDKIOSWebTests/MFATypeTests.swift
+++ Projects/AccountSDKIOSWeb/Tests/AccountSDKIOSWebTests/MFATypeTests.swift
@@ -6,13 +6,9 @@ final class MFATypeTests: XCTestCase {
     func testReturnsCorrectValuesForEidOnPre() {
         let eidSe : MFAType = .preEid(.se)
         let eidNo : MFAType = .preEid(.no)
-        let eidFi : MFAType = .preEid(.fi)
-        let eidDk : MFAType = .preEid(.dk)
         
         XCTAssertEqual(eidSe.rawValue, PreEidType.se.rawValue)
         XCTAssertEqual(eidNo.rawValue, PreEidType.no.rawValue)
-        XCTAssertEqual(eidFi.rawValue, PreEidType.fi.rawValue)
-        XCTAssertEqual(eidDk.rawValue, PreEidType.dk.rawValue)
     }
     
 }
