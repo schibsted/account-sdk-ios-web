a4cc2785f49a1fe028f794f4ad2be43b69935f2d
Anna Zamora <anna.zamora@schibsted.com>
2022-04-20 13:29:07 +0200

    1882 Fix typo

--- Sources/AccountSDKIOSWeb/Client/Client.swift
+++ Sources/AccountSDKIOSWeb/Client/Client.swift
@@ -136,7 +136,7 @@ public class Client: CustomStringConvertible {
                                   httpClient: chttpClient,
                                   jwks: jwks,
                                   tokenHandler: tokenHandler)
-        let clousure = { authCode, authState, completion in
+        let closure = { authCode, authState, completion in
             tokenHandler.makeTokenRequest(authCode: authCode,
                                           authState: authState,
                                           completion: completion)
@@ -145,7 +145,7 @@ public class Client: CustomStringConvertible {
                                                             to: newSessionStorage,
                                                             legacyClient: legacyClient,
                                                             legacyClientSecret: sessionStorageConfig.legacyClientSecret,
-                                                            makeTokenRequest: clousure)
+                                                            makeTokenRequest: closure)
 
         self.init(configuration: configuration,
                   sessionStorage: sessionStorage,
