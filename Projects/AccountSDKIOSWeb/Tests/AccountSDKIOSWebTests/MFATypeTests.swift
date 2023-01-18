621cc81218c3fd80a626d6bb72bf250a3d8e4e2f
Samuel Gulliksson <zamzterz@users.noreply.github.com>
2023-01-18 16:14:39 +0100

    Support starting Finnish eID flow in PRE. (#206)

--- Projects/AccountSDKIOSWeb/Tests/AccountSDKIOSWebTests/MFATypeTests.swift
+++ Projects/AccountSDKIOSWeb/Tests/AccountSDKIOSWebTests/MFATypeTests.swift
@@ -11,9 +11,11 @@ final class MFATypeTests: XCTestCase {
     func testReturnsCorrectValuesForEidOnPre() {
         let eidSe : MFAType = .preEid(.se)
         let eidNo : MFAType = .preEid(.no)
+        let eidFi : MFAType = .preEid(.fi)
         
         XCTAssertEqual(eidSe.rawValue, PreEidType.se.rawValue)
         XCTAssertEqual(eidNo.rawValue, PreEidType.no.rawValue)
+        XCTAssertEqual(eidFi.rawValue, PreEidType.fi.rawValue)
     }
     
 }
