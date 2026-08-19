aefffd1aa99052bb10fca8bc36467d3ae2d3a25b
Claus Jørgensen <claus.joergensen@schibsted.com>
2026-08-19 15:15:46 +0200

    Update AuthenticatedURLSessionTests.swift

--- Tests/SchibstedAccountTests/AuthenticatedURLSessionTests.swift
+++ Tests/SchibstedAccountTests/AuthenticatedURLSessionTests.swift
@@ -325,12 +325,12 @@ struct AuthenticatedURLSessionTests {
             }
 
             await withCheckedContinuation { continuation in
-await withCheckedContinuation { continuation in
-                let dataTask = authenticatedURLSession.dataTask(with: URLRequest(url: requestURL)) { _, _, _ in
-                    continuation.resume()
+                await withCheckedContinuation { continuation in
+                    let dataTask = authenticatedURLSession.dataTask(with: URLRequest(url: requestURL)) { _, _, _ in
+                        continuation.resume()
+                    }
+                    dataTask.resume()
                 }
-                dataTask.resume()
-            }
             }
         }
 
